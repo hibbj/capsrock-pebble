e93e368a93b5b6e543a4ec84d0228d20483b6cb4
hibbj <hibbj916@gmail.com>
2013-10-30 19:21:19 -0700

    Started a test project

---
+++ src/main.c
@@ -0,0 +1,36 @@
+#include "pebble_os.h"
+#include "pebble_app.h"
+#include "pebble_fonts.h"
+
+
+#define MY_UUID { 0x08, 0xB8, 0xED, 0x0C, 0xD3, 0xC1, 0x49, 0xC6, 0x88, 0xBA, 0xAD, 0x34, 0xF0, 0x73, 0x2E, 0x94 }
+PBL_APP_INFO(MY_UUID,
+             "Hello World!", "CAPSROCK",
+             1, 0, /* App version */
+             DEFAULT_MENU_ICON,
+             APP_INFO_STANDARD_APP);
+
+Window window;
+TextLayer hello_layer;
+
+
+void handle_init(AppContextRef ctx) {
+  (void)ctx;
+
+  window_init(&window, "Window Name");
+  window_stack_push(&window, true /* Animated */);
+	
+  text_layer_init(&hello_layer, GRect(0, 65, 144, 30));
+  text_layer_set_text_alignment(&hello_layer, GTextAlignmentCenter);
+  text_layer_set_text(&hello_layer, "Hello World!");
+  text_layer_set_font(&hello_layer, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
+  layer_add_child(&window.layer, &hello_layer.layer);
+}
+
+
+void pbl_main(void *params) {
+  PebbleAppHandlers handlers = {
+    .init_handler = &handle_init
+  };
+  app_event_loop(params, &handlers);
+}
